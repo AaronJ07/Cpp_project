e8d08e580f5afd52d1af9cce846c6d67003eee1f
Zhi Hong <66683692+AaronJ07@users.noreply.github.com>
2025-11-08 22:21:37 +0800

    Create q2.cpp

---
+++ study/geeksforgeeks/q2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int read_print(int n)
+{
+    cout << "Please enter a number: ";
+    cin >> n;
+    cout << "The number you input is: " << n;
+}
+
+int main()
+{
+    int n;
+    read_print(n);
+
+    return 0;
+}
