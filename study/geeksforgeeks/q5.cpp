1410dcafcf4b1a7e14cf9aae2f95ddf92558f536
Zhi Hong <66683692+AaronJ07@users.noreply.github.com>
2025-11-09 15:40:40 +0800

    Create q5.cpp

---
+++ study/geeksforgeeks/q5.cpp
@@ -0,0 +1,19 @@
+// Write a Program to Find the Size of int, float, double, and char.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    float b;
+    double c;
+    char d;
+
+    cout << " Size of int: " << sizeof(a) << " bytes" << endl;
+    cout << " Size of float: " << sizeof(b) << " bytes" << endl;
+    cout << " Size of double: " << sizeof(c) << " bytes" << endl;
+    cout << " Size of char: " << sizeof(d) << " bytes" << endl;
+
+    return 0;
+}
