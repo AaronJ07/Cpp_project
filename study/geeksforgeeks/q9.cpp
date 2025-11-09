04d7378ac94e0852c230a9ca39d2e964fffcde26
Zhi Hong <66683692+AaronJ07@users.noreply.github.com>
2025-11-09 17:51:45 +0800

    Update q9.cpp

--- study/geeksforgeeks/q9.cpp
+++ study/geeksforgeeks/q9.cpp
@@ -1,1 +1,40 @@
+// Write a Program to Check if a Given Year Is a Leap Year.
 
+#include<iostream>
+using namespace std;
+
+bool check_year(int year)
+{
+    if(year % 400 == 0)
+    {
+        return true;
+    }
+    else if(year %100 != 0)
+    {
+        return true;
+    }
+    else if(year % 4 == 0)
+    {
+        return true;
+    }
+}
+
+int main()
+{
+    int year;
+    cout << "Please enter a year: ";
+    cin >> year;
+
+    check_year(year);
+
+    if(true)
+    {
+        cout << year << " is a Leap Year." << endl;
+    }
+    else if(false)
+    {
+        cout << year << " not a Leap Year." << endl;
+    }
+
+    return 0;
+}
