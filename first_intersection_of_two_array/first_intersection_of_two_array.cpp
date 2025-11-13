1ef5c1d581456f057eef11a007420b05d3157cf5
Zhi Hong <j76541512@gmail.com>
2025-11-13 21:49:16 +0800

    Update first_intersection_of_two_array.cpp

---
+++ first_intersection_of_two_array/first_intersection_of_two_array.cpp
@@ -0,0 +1,19 @@
+/*
+
+You are given two arrays of integers:
+
+arr1 = [1, 2, 3, 4, 5, 6]
+arr2 = [7, 8, 9, 0, 5, 6]
+
+
+Write a function that finds and prints the first element that intersect in both arrays.
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+}
