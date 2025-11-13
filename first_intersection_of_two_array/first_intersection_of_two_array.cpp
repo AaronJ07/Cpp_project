01e730fe6e83f4a696f6e231498a5baa79764b33
Zhi Hong <j76541512@gmail.com>
2025-11-13 22:15:41 +0800

    Update first_intersection_of_two_array.cpp

--- first_intersection_of_two_array/first_intersection_of_two_array.cpp
+++ first_intersection_of_two_array/first_intersection_of_two_array.cpp
@@ -13,7 +13,19 @@ Write a function that finds and prints the first element that intersect in both 
 #include<iostream>
 using namespace std;
 
-int main()
+const int ARR_SIZE = 6;
+
+void find_first_intersection(int* arr1, int* arr2)
 {
     
+}
+
+int main()
+{
+    int arr1[ARR_SIZE] = {1, 2, 3, 4, 5, 6};
+    int arr2[ARR_SIZE] = {7, 8, 9, 0, 5, 6};
+
+    find_first_intersection(arr1, arr2);
+
+    return 0;
 }
