a6f803474cbb2d3ccd4450e6be22d9e11c02dfc4
Zhi Hong <j76541512@gmail.com>
2025-11-14 17:41:34 +0800

    Update multiple_two_numbers_using_addition.cpp

--- multiple_two_numbers_using_addition/multiple_two_numbers_using_addition.cpp
+++ multiple_two_numbers_using_addition/multiple_two_numbers_using_addition.cpp
@@ -10,3 +10,11 @@ Write a function that computes the product of a and b using only the + operator.
 You are not allowed to use *, /, or built-in multiplication functions.
 
 */
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int 
+}
