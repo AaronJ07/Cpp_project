dc2a0375362b299280839a5711a15f1f6db0c737
Zhi Hong <j76541512@gmail.com>
2025-11-14 17:15:56 +0800

    Create multiple_two_numbers_using_addition.cpp

---
+++ multiple_two_numbers_using_addition/multiple_two_numbers_using_addition.cpp
@@ -0,0 +1,12 @@
+/*
+
+You are given two integers, for example:
+
+Input: 7 , 5
+Output: 35
+
+
+Write a function that computes the product of a and b using only the + operator.
+You are not allowed to use *, /, or built-in multiplication functions.
+
+*/
